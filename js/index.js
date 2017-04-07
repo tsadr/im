a950694b8cba169fb8384d629589d6880a7e88e3
M.Sadr <tsadr@voyager.xyz>
2017-04-07 18:56:51 +0430

    Date Changed

--- js/index.js
+++ js/index.js
@@ -37,5 +37,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date("April 21, 2017 01:00:00");
+var deadline = new Date("April 27, 2017 00:00:00");
 initializeClock('clockdiv', deadline);
