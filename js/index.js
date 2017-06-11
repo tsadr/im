e319a4e7956b67908edaa7902d9aac501f9005c1
M.Sadr <tsadr@voyager.xyz>
2017-06-11 18:29:12 +0430

    Change Date for Final Result

--- js/index.js
+++ js/index.js
@@ -40,5 +40,5 @@ function initializeClock(id, endtime) {
   var timeinterval = setInterval(updateClock, 1000);
 }
 
-var deadline = new Date("June 6, 2017 10:00:00");
+var deadline = new Date("September 1, 2017 15:00:00");
 initializeClock('clockdiv', deadline);
